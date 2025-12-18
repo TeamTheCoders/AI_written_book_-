9f9488a6ee012e58bdff4b3b4bccaa297e368884
TeamTheCoders <new2200527@gmail.com>
2025-12-18 21:56:37 +0500

    Update docusaurus.config.ts

--- frontend/docusaurus.config.ts
+++ frontend/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://TeamTheCoders.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AI_written_book_-/frontend/',
+  baseUrl: '/AI_written_book_-/',
 
   customFields: {
     backendUrl: 'http://localhost:8000', // Default for local development
